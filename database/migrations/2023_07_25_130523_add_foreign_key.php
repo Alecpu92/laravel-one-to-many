81b1900b33952199db9184506e95e05b3f71d973
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-07-25 15:23:57 +0200

    lavorati up e down della migration

--- database/migrations/2023_07_25_130523_add_foreign_key.php
+++ database/migrations/2023_07_25_130523_add_foreign_key.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema:: table('projects', function (Blueprint $table) {
 
+            $table -> foreignId('type_id') -> constrained();
+
     });
     }
 
@@ -25,6 +27,11 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema:: table('projects', function (Blueprint $table) {
+
+            $table -> dropForeign('projects_type_id_foreign');
+            $table -> dropColumn('type_id');
+
+    });
     }
 };
